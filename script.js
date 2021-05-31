809929712d47d6577621cb84225dfd4021f47c52
rishika-kotla <79087358+rishika-kotla@users.noreply.github.com>
2021-05-31 13:46:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let ds = document.getElementsByClassName('.datastructures-div')
+let but = document.getElementsByClassName('.datastructures')    
+ function dropdown(){
+    ds.style.display = 'flex'
+    console.log('clicked..')
+}
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
